d4abdb0505164ea58275f1df3bedbb6531ea39d9
Егор Павшук <egor.pavshuk@outlook.com>
2021-08-17 21:24:42 +0300

    Created project

---
+++ GroupProject/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GroupProject
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+            {
+                Database.EnsureCreated();
+            }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Test;Username=postgres;Password=Egor1324");
+            }
+    }
+}
