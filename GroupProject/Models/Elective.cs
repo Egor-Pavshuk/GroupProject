a7bb17d1c374bc5c994aff01e6c09cc35fad8f2a
Егор Павшук <egor.pavshuk@outlook.com>
2021-08-18 15:58:26 +0300

    Created models

---
+++ GroupProject/Models/Elective.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GroupProject.Models
+{
+    public class Elective
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
