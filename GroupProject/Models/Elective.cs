0a9cab03283319a19a1f83cfb75bcea14e69ff50
Егор Павшук <egor.pavshuk@outlook.com>
2021-08-22 16:44:20 +0300

    Removed useless classes

--- GroupProject/Models/Elective.cs
+++ GroupProject/Models/Elective.cs
@@ -8,38 +8,5 @@ namespace GroupProject.Models
         public string Name { get; set; }
         public List<Student> Students { get; set; }
     }
-
-
-
-    abstract class Animal
-    {
-        private string _name;
-
-        public abstract void Byte();
-    }
-
-    class Dog : Animal
-    {
-        public override void Byte()
-        {
-            
-        }
-
-        public void Run()
-        {
-            
-        }
-    }
-
-    class MyClass
-    {
-        private void DoSomething()
-        {
-            List<Animal> animals = new List<Animal>();
-            Dog dog = new Dog();
-            animals.Add(dog);
-            animals[0].Byte();
-        }
-    }
 }
 
