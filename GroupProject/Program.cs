61e268e72db6500f41f16da1de7750ba9e89e0f5
Егор Павшук <egor.pavshuk@outlook.com>
2021-08-22 16:43:17 +0300

    Created methods for CRUD

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using GroupProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GroupProject
 {
@@ -51,8 +52,9 @@ namespace GroupProject
             };
             students[0].Electives = new List<Elective>() {elective2};
             students[1].Electives = new List<Elective>() {elective1};
-            students[3].Electives = new List<Elective>() {elective2};
             students[2].Electives = new List<Elective>() {elective1};
+            students[3].Electives = new List<Elective>() {elective2};
+            
 
             using (ApplicationContext db = new ApplicationContext())
             {
@@ -62,26 +64,35 @@ namespace GroupProject
                 db.SaveChanges();
             }
             
-            /*using (ApplicationContext db = new ApplicationContext())
+            var studentsFromDb = GetStudents(); //todo rename
+            ShowStudentsInformation(studentsFromDb);
+            ChangingStudentName();
+            ChangingStudentGroup();
+            DeletingElective();
+
+            studentsFromDb = GetStudents(); //todo rename
+            ShowStudentsInformation(studentsFromDb);
+           
+            using (ApplicationContext db = new ApplicationContext())
             {
-                var all = from e in db.Electives select e;
-                db.Electives.RemoveRange(all);
+                var allElectives = from e in db.Electives select e;
+                db.Electives.RemoveRange(allElectives);
                 
-                var all1 = from s in db.Students select s;
-                db.Students.RemoveRange(all1);
+                var allStudents = from s in db.Students select s;
+                db.Students.RemoveRange(allStudents);
                 
-                var all2 = from g in db.Groups select g;
-                db.Groups.RemoveRange(all2);
+                var allGroups = from g in db.Groups select g;
+                db.Groups.RemoveRange(allGroups);
                 
                 db.SaveChanges();
-                var el = new [] {db.Electives.ToList().Count, db.Students.ToList().Count, db.Groups.ToList().Count};
+                var allElements = new [] {db.Electives.ToList().Count, db.Students.ToList().Count, db.Groups.ToList().Count};
                 
-                Console.WriteLine($"{el[0]}, {el[1]}, {el[2]}");
+                Console.WriteLine($"{allElements[0]}, {allElements[1]}, {allElements[2]}");
                 Console.ReadKey();
-            }*/
+            }
                 
 
-                Console.ReadKey();
+            Console.ReadKey();
         }
 
         static void AddEntityToDb<T>(T entity) //todo 
@@ -91,6 +102,94 @@ namespace GroupProject
                 db.Add(entity);
                 db.SaveChanges();
             }
+        }
+
+        static void ChangingStudentName()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var student = db.Students.FirstOrDefault(s => s.Name == "Rick");
+                if (student != null)
+                {
+                    student.Name = "Moran";
+                    student.Surname = "Corn";
+                }
+
+                db.SaveChanges();
+            }
+        }
+
+        static void ChangingStudentGroup()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var student = db.Students
+                    .Include(s => s.Group)
+                    .Include(s => s.Electives)
+                    .FirstOrDefault(s => s.Name == "Frank");
+                var firstGroup = db.Groups
+                    .Include(g => g.Students).FirstOrDefault(g => g.Id == student.GroupId);
+                var secondGroup = db.Groups
+                    .Include(g => g.Students).FirstOrDefault(g => g.Id != student.GroupId);
+                
+                firstGroup?.Students.Remove(student);
+                secondGroup?.Students.Add(student);
+                if (student != null)
+                {
+                    student.Group = secondGroup;
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        static void DeletingElective()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var elective = db.Electives.FirstOrDefault(e => e.Name == "Biology");
+                if (elective != null) 
+                    db.Electives.Remove(elective);
+                db.SaveChanges();
+            }
+        }
+        
+        static List<Student> GetStudents()
+        {
+            List<Student> students;
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                students = db.Students.Include(s => s.Electives)
+                    .Include(s => s.Group).ToList();
+            }
+            return students;
+        }
+
+        static void ShowStudentsInformation(List<Student> students)
+        {
+            var orderedStudents = students.OrderBy(s => s.GroupId).ToList();
+            int preGroupId = orderedStudents[0].GroupId;
+            Console.WriteLine();
+            Console.WriteLine($"Group: {orderedStudents[0].Group.Name}");
+            foreach (var student in orderedStudents)
+            {
+                int groupId = student.GroupId;
+                if (groupId != preGroupId)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Group: {student.Group.Name}");
+                    
+                    preGroupId = groupId;
+                }
+                Console.Write($"{student.Name} {student.Surname} : ");
+                foreach (var elective in student.Electives)
+                {
+                    Console.Write($"{elective.Name} ");
+                    
+                }
+                Console.WriteLine();
+
+            }
+            
         }
     }
 }
